e97b674e1501107fde3216fb06298fb00a61c325
chairowell <chairowell@gmail.com>
2026-07-16 12:16:18 +0800

    🐳 chore(theme): 移除文档主题中冗余的组件导入和样式引入
    删除了不再需要的全局组件注册代码和样式导入文件，清理文档主题初始化代码

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,6 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './style.scss'
-
-import '../../../src/d-button'
-import '../../../src/confirm-button'
-import '../../../src/d-icon'
 
 export default {
   extends: DefaultTheme,
